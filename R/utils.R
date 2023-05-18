8d278a9799915e34e734b15f38182485b8a41c6a
howardbaek <howardbaek.fh@gmail.com>
2023-05-17 21:29:49 -0600

    Add `play_audio()`

--- R/utils.R
+++ R/utils.R
@@ -8,3 +8,26 @@ get_env_key = function(x) {
   }
   return(x)
 }
+
+#' Play audio in a browser
+#'
+#' This uses HTML5 audio tags to play audio in your browser. Borrowed from \code{googleLanguageR::gl_talk_player()}
+#'
+#' @param audio The file location of the audio file.  Must be supported by HTML5
+#' @param html The html file location that will be created host the audio
+#'
+#' @export
+#' @importFrom utils browseURL
+play_audio <- function(audio = "output.wav",
+                       html = "player.html"){
+  # Write html code to a html file
+  writeLines(sprintf('<html><body>
+    <audio controls autoplay>
+                     <source src="%s">
+                     </audio>
+                     </body></html>',
+                     audio),
+             html)
+  # Load URL into browser
+  utils::browseURL(html)
+}
